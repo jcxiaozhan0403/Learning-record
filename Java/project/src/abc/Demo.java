4f80c65df5c56024bd29a207cf9d5ea2afb1fdec
John.Cena <349636607@qq.com>
2020-09-16 11:10:45 +0800

    java

--- Java/project/src/abc/Demo.java
+++ Java/project/src/abc/Demo.java
@@ -2,6 +2,12 @@ package abc;
 
 public class Demo {
     public static void main(String[] args) {
+        int i = 0;
+        int k = 1;
+        int x = 1;
 
+//        System.out.println(i != 0 & ++k ==2);
+//        System.out.println(i != 0 && ++k ==2);
+        System.out.println(x++ == 2);
     }
 }
