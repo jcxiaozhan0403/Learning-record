1ec0d881b5b4cd6a3d25cc5779b107e010c4b8b6
John.Cena <349636607@qq.com>
2020-11-02 22:37:08 +0800

    java

---
+++ Java/H2J/src/controlProcess/Judge.java
@@ -0,0 +1,12 @@
+package controlProcess;
+
+public class Judge {
+    public static void main(String[] args) {
+        boolean x = true;
+        if(x){
+            System.out.println("true");
+        }else{
+            System.out.println("false");
+        }
+    }
+}
