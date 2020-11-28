8a09ac7e302c37b94d5ccccf770ffb239677eb20
John.Cena <349636607@qq.com>
2020-11-28 10:28:02 +0800

    java

--- Java/H2J/Java笔记(自学).md
+++ Java/H2J/Java笔记(自学).md
@@ -527,4 +527,37 @@ int i= Integer.parseInt(x);
 %d 表示数字
 %n 表示换行
 
+printf和format能够达到一模一样的效果
+
+printf中是直接调用了format
+
+数字的格式化输出
+```java
+public class TestNumber {
+   
+    public static void main(String[] args) {
+        int year = 2020;
+          
+        //直接打印数字
+        System.out.format("%d%n",year);
+        //总长度是8,默认右对齐
+        System.out.format("%8d%n",year);
+        //总长度是8,左对齐
+        System.out.format("%-8d%n",year);
+        //总长度是8,不够补0
+        System.out.format("%08d%n",year);
+        //千位分隔符
+        System.out.format("%,8d%n",year*10000);
+  
+        //小数点位数
+        System.out.format("%.2f%n",Math.PI);
+          
+        //不同国家的千位分隔符
+        System.out.format(Locale.FRANCE,"%,.2f%n",Math.PI*10000);
+        System.out.format(Locale.US,"%,.2f%n",Math.PI*10000);
+        System.out.format(Locale.UK,"%,.2f%n",Math.PI*10000);
+    }
+}
+```
+
 ## 问题 ：匿名类
--- Java/H2J/src/Demo.java
+++ Java/H2J/src/Demo.java
@@ -1,29 +1,32 @@
 import java.util.Locale;
+import java.util.Scanner;
 
 public class Demo{
     public static void main(String[] args) {
-        int year = 2020;
-        //总长度，左对齐，补0，千位分隔符，小数点位数，本地化表达
+        Scanner s = new Scanner(System.in);
 
-        //直接打印数字
-        System.out.format("%d%n",year);
-        //总长度是8,默认右对齐
-        System.out.format("%8d%n",year);
-        //总长度是8,左对齐
-        System.out.format("%-8d%n",year);
-        //总长度是8,不够补0
-        System.out.format("%08d%n",year);
-        //千位分隔符
-        System.out.format("%,8d%n",year*10000);
+        System.out.println("请输入地名");
+        String countries = s.nextLine();
 
-        //小数点位数
-        System.out.format("%.2f%n",Math.PI);
+        System.out.println("请输入公司类型");
+        String companyType = s.toString();
 
-        //不同国家的千位分隔符
-        System.out.format(Locale.FRANCE,"%,.2f%n",Math.PI*10000);
-        System.out.format(Locale.US,"%,.2f%n",Math.PI*10000);
-        System.out.format(Locale.UK,"%,.2f%n",Math.PI*10000);
-        System.out.format(Locale.CHINA,"%,.2f%n",Math.PI*10000);
+        System.out.println("请输入老板名称");
+        String bossName = s.nextLine();
 
+        System.out.println("请输入公司名称");
+        String companyName = s.nextLine();
+
+        System.out.println("请输入产品");
+        String product = s.nextLine();
+
+        System.out.println("请输入计量单位");
+        String unit = s.nextLine();
+
+        System.out.println("请输入金额");
+        String amount = s.nextLine();
+
+        System.out.println("%s最大%s%s倒闭了，王八蛋老板%s吃喝嫖赌，欠下了%s个亿，带着他的小姨子跑了!我们没有办法，" +
+                "拿着%s抵工资!原价都是一%s多、两%s多、三%s多的%s，现在全部只卖二十块，统统只要二十块!%s王八蛋，你不是人!我们辛辛苦苦给你干了大半年，你不发工资，你还我血汗钱，还我血汗钱!",countries,companyType,);
     }
 }
