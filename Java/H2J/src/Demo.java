1ecd6681aa619ea0771351174feafd436823a07a
John.Cena <349636607@qq.com>
2020-12-16 20:45:34 +0800

    java

--- Java/H2J/src/Demo.java
+++ Java/H2J/src/Demo.java
@@ -1,9 +1,10 @@
+import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
-public class Demo {
+public class Demo implements ActionListener {
     public static void main(String[] args) {
         String s;
         System.out.println("s=" + s);
