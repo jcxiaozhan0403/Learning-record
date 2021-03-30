156ea8ec835b74ccaed499fb50a166dd8dbb602b
John.Cena <349636607@qq.com>
2021-03-30 22:34:32 +0800

    java

---
+++ SSM/test/src/main/java/cn/com/scitc/test/dao/UserDao.java
@@ -0,0 +1,9 @@
+package cn.com.scitc.test.dao;
+
+import cn.com.scitc.test.pojo.User;
+
+import java.util.List;
+
+public interface UserDao {
+    List<User> getUserList();
+}
---
+++ SSM/test/src/main/java/cn/com/scitc/test/utils/MybatisUtils.java
@@ -0,0 +1,27 @@
+package cn.com.scitc.test.utils;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class MybatisUtils {
+    private static SqlSessionFactory sqlSessionFactory;
+
+    static {
+        InputStream inputStream = null;
+        try {
+            inputStream = Resources.getResourceAsStream("mybatis-config.xml");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+    }
+
+    public static SqlSession getSqlSession(){
+        return sqlSessionFactory.openSession();
+    }
+}
