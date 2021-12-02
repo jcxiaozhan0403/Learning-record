8710697d15ee63ccd99d39ac229fa32d428e42d6
John.Cena <349636607@qq.com>
2021-12-02 17:40:39 +0800

    c

--- C/demo01.c
+++ C/demo01.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
 
-//函数原型 
-void sum(int a,int b);
-
 int main()
 {	
-	//函数调用
-	sum(99,88);	
+	int i = 99;
+	int j = 100;
+	int* const q = &i;
+	q++; 
+	
+	printf("%d",*q);
+	printf("---------------------");
+	printf("%x",&j);
+	
+	
 	return 0; 
-}
-
-void sum(int a,int b){
-	printf("%d",a+b);
 }
