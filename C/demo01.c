ebaab32684e3eef90d405145d0b1e23b52dd8dc3
John.Cena <349636607@qq.com>
2021-12-10 11:20:31 +0800

    c

--- C/demo01.c
+++ C/demo01.c
@@ -2,6 +2,14 @@
 
 int main()
 {	
-	printf("%9.2f",123.456);
+	FILE *fp = fopen("D:/\Study/\Learning-record/\C/\demo.txt","r");
+	if(fp) {
+		int num;
+		fscanf(fp,"%d",&num);
+		printf("%d\n",num);
+		fclose(fp);
+	}else {
+		printf("无法打开文件\n");
+	}
 	return 0;
 }
