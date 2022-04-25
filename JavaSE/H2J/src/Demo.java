0385439d9a0de013ea2f6be0fce82c979bdc9c77
John.Cena <349636607@qq.com>
2022-04-25 17:31:23 +0800

    java

--- JavaSE/H2J/src/Demo.java
+++ JavaSE/H2J/src/Demo.java
@@ -1,5 +1,7 @@
 public class Demo{
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Demo demo = new Demo();
+        int a = 10,b = 20;
+        System.out.println(a + b + "a");
     }
 }
