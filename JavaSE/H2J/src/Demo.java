a7a5287054c3c70bb978f59393ff173b6a17d0e8
John.Cena <349636607@qq.com>
2022-04-26 22:41:36 +0800

    java

--- JavaSE/H2J/src/Demo.java
+++ JavaSE/H2J/src/Demo.java
@@ -1,7 +1,20 @@
 public class Demo{
     public static void main(String[] args) {
-        Demo demo = new Demo();
-        int a = 10,b = 20;
-        System.out.println(a + b + "a");
+
+
+        for (int i = 0; i < 6; i++) {
+            for (int i1 = 6-i; i1 > 0; i1--) {
+                System.out.print(" ");
+            }
+            for (int i2 = 6-i; i2 < 6; i2++) {
+                System.out.print("*");
+            }
+            for (int i3 = 5-i; i3 < 6; i3++) {
+                System.out.print("*");
+            }
+
+            System.out.print("\n");
+        }
+
     }
 }
