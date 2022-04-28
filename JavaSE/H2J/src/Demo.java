8286747cdd746fc0868ec554528a90632783b350
John.Cena <349636607@qq.com>
2022-04-29 02:23:47 +0800

    java

--- JavaSE/H2J/src/Demo.java
+++ JavaSE/H2J/src/Demo.java
@@ -1,16 +1,10 @@
 public class Demo{
-    public static void print(String... names) {
-        int count = names.length;    // 获取总个数
-        System.out.println("本次参加考试的有"+count+"人，名单如下：");
-        for(int i = 0;i < names.length;i++) {
-            System.out.println(names[i]);
-        }
-    }
-
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
+        int a[];
+        a = new int[11];
+        a[8] = 20;
+        System.out.println(a[9]);
+        System.out.println(a[8]);
 
-        print("张强","李成","王勇");    // 传入3个值
-        print("马丽","陈玲");
     }
 }
