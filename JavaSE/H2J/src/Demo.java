8a6c2bcfa7682ed82db3506b2b887d52646b9d5a
John.Cena <349636607@qq.com>
2022-06-12 15:40:55 +0800

    update

--- JavaSE/H2J/src/Demo.java
+++ JavaSE/H2J/src/Demo.java
@@ -1,10 +1,3 @@
 public class Demo{
-    public static void main(String[] args) {
-        int a[];
-        a = new int[11];
-        a[8] = 20;
-        System.out.println(a[9]);
-        System.out.println(a[8]);
 
-    }
 }
