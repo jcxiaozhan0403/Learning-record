a1df55b61e401586ba8c048c8412fc8c4199a6fa
JohnCena <349636607@qq.com>
2022-10-30 23:11:45 +0800

    update

---
+++ JavaSE/H2J/src/People.java
@@ -0,0 +1,9 @@
+/**
+ * @author John.Cena
+ * @date 2022/10/30 23:07
+ * @Description:
+ */
+public class People {
+
+
+}
--- JavaSE/H2J/src/Test.java
+++ JavaSE/H2J/src/Test.java
@@ -1,5 +1,6 @@
 public class Test {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        Student student = new Student();
+        System.out.println(student instanceof People);
     }
 }
