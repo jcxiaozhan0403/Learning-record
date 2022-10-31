813e7f9c407d12821242abc769d7263619a4a51a
JohnCena <349636607@qq.com>
2022-10-31 15:04:30 +0800

    update

--- JavaSE/H2J/src/Test.java
+++ JavaSE/H2J/src/Test.java
@@ -4,6 +4,5 @@ public class Test {
     public static void main(String[] args) {
         Student student = new Student();
         System.out.println(student instanceof People);
-        Arrays.
     }
 }
