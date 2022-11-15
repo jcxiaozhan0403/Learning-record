83e1b8af20bee3ea80f79593756e94d1c5651aaf
JohnCena <349636607@qq.com>
2022-11-15 19:00:42 +0800

    update

--- JavaSE/H2J/src/Test.java
+++ JavaSE/H2J/src/Test.java
@@ -1,3 +1,17 @@
 public class Test {
+    public static void main(String[] args) {
+        int x = 1,a = 3;
+        if (a == 1){
+            x += 5;
+        }else if (a == 2){
+            x += 10;
+        }else if(a == 3){
+            x += 16;
+        }else if (a == 4){
+            x +=34;
+        }
+        System.out.println(x);
 
+
+    }
 }
