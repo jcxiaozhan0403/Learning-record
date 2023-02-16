1afa415c22a77f5c9565a03d21a4a4b1135a01b1
JohnCena <349636607@qq.com>
2023-02-16 21:26:29 +0800

    update

---
+++ JavaSE/H2J/src/SS1.java
@@ -0,0 +1,12 @@
+/**
+ * @author John.Cena
+ * @date 2023/2/16 20:59
+ * @Description:
+ */
+public interface SS1 {
+    int getNum();
+
+    default void demo(){
+        System.out.println("你好");
+    }
+}
---
+++ JavaSE/H2J/src/SS2.java
@@ -0,0 +1,16 @@
+/**
+ * @author John.Cena
+ * @date 2023/2/16 21:00
+ * @Description:
+ */
+public class SS2 implements SS1{
+    @Override
+    public int getNum() {
+        return 0;
+    }
+
+    @Override
+    public void demo() {
+        SS1.super.demo();
+    }
+}
