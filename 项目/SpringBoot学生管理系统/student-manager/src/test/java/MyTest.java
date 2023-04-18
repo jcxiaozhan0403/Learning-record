0cb6a0fbe6d129579bbb47ac3095fe6413bafe1f
JohnCena <349636607@qq.com>
2023-04-18 22:20:53 +0800

    update

---
+++ 项目/SpringBoot学生管理系统/student-manager/src/test/java/MyTest.java
@@ -0,0 +1,12 @@
+import java.util.Hashtable;
+
+/**
+ * @author John.Cena
+ * @date 2023/4/18 21:28
+ * @Description:
+ */
+public class MyTest {
+    public static void main(String[] args) {
+        Hashtable
+    }
+}
