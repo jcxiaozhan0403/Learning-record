16400d87726f326dc436efba0555e5134be7ab8f
JohnCena <349636607@qq.com>
2023-06-01 00:44:27 +0800

    update

--- C++/cpp_project/test8.cpp
+++ C++/cpp_project/test8.cpp
@@ -1,49 +1,52 @@
 #include <iostream>
-#include <ctime>
-#include <cstdlib>
+#include <vector>
+#include <random>
+#include <chrono>
 using namespace std;
-
-// 打印数组
-void printArray(int arr[], int size) {
-    for (int i = 0; i < size; i++) {
-        cout << arr[i] << " ";
-    }
-    cout << endl;
-}
+using namespace chrono;
 
 // 直接插入排序
-void insertionSort(int arr[], int size) {
-    int i, key, j;
-    for (i = 1; i < size; i++) {
-        key = arr[i];
-        j = i - 1;
-
+void insertionSort(vector<int>& arr) {
+    int n = arr.size();
+    for (int i = 1; i < n; ++i) {
+    	// 选取当前元素作为插入元素
+        int key = arr[i];
+        int j = i - 1;
+        // 向前查找插入位置并移动元素
         while (j >= 0 && arr[j] > key) {
             arr[j + 1] = arr[j];
-            j = j - 1;
+            j--;
         }
+        // 插入元素到正确位置
         arr[j + 1] = key;
     }
 }
 
 // 希尔排序
-void shellSort(int arr[], int size) {
-    for (int gap = size / 2; gap > 0; gap /= 2) {
-        for (int i = gap; i < size; i++) {
+void shellSort(vector<int>& arr) {
+    int n = arr.size();
+    // 根据增量进行分组
+    for (int gap = n / 2; gap > 0; gap /= 2) {
+        for (int i = gap; i < n; ++i) {
+        	// 选取当前元素作为插入元素
             int temp = arr[i];
             int j;
+            // 向前查找插入位置并移动元素
             for (j = i; j >= gap && arr[j - gap] > temp; j -= gap) {
                 arr[j] = arr[j - gap];
             }
+            // 插入元素到正确位置
             arr[j] = temp;
         }
     }
 }
 
 // 冒泡排序
-void bubbleSort(int arr[], int size) {
-    for (int i = 0; i < size - 1; i++) {
-        for (int j = 0; j < size - i - 1; j++) {
+void bubbleSort(vector<int>& arr) {
+    int n = arr.size();
+    for (int i = 0; i < n - 1; ++i) {
+    	// 依次比较相邻的元素并交换
+        for (int j = 0; j < n - i - 1; ++j) {
             if (arr[j] > arr[j + 1]) {
                 swap(arr[j], arr[j + 1]);
             }
@@ -51,94 +54,82 @@ void bubbleSort(int arr[], int size) {
     }
 }
 
-// 快速排序中的分区操作
-int partition(int arr[], int low, int high) {
-    int pivot = arr[high];
-    int i = (low - 1);
-
-    for (int j = low; j <= high - 1; j++) {
-        if (arr[j] <= pivot) {
+// 快速排序
+int partition(vector<int>& arr, int low, int high) {
+	// 选取最后一个元素作为基准
+    int pivot = arr[high]; 
+    int i = low - 1;
+    for (int j = low; j <= high - 1; ++j) {
+    	// 将小于基准的元素放到左边
+        if (arr[j] < pivot) {
             i++;
             swap(arr[i], arr[j]);
         }
     }
+    // 将基准放到正确位置
     swap(arr[i + 1], arr[high]);
     return (i + 1);
 }
 
-// 快速排序
-void quickSort(int arr[], int low, int high) {
+void quickSort(vector<int>& arr, int low, int high) {
     if (low < high) {
+    	// 划分数组，并获取基准的位置
         int pi = partition(arr, low, high);
-
+        // 递归地对左半部分进行快速排序
         quickSort(arr, low, pi - 1);
+        // 递归地对右半部分进行快速排序
         quickSort(arr, pi + 1, high);
     }
 }
 
-int main() {
-    const int size = 500;
-    int arr[size];
-
-    srand(time(0));
-
-    // 生成随机数组
-    for (int i = 0; i < size; i++) {
-        arr[i] = rand() % 2000 + 1;
+// 生成随机数
+vector<int> generateRandomNumbers(int count, int minValue, int maxValue) {
+    vector<int> numbers(count);
+    random_device rd;
+    mt19937 gen(rd());
+    uniform_int_distribution<int> dis(minValue, maxValue);
+    for (int i = 0; i < count; ++i) {
+        numbers[i] = dis(gen);
     }
+    return numbers;
+}
 
-    cout << "原始数组：" << endl;
-    printArray(arr, size);
+int main() {
+    // 生成随机数
+    vector<int> numbers = generateRandomNumbers(500, 1, 2000);
 
     // 直接插入排序
-    clock_t start = clock();
-    insertionSort(arr, size);
-    clock_t finish = clock();
-    double insertionSortTime = double(finish - start) / CLOCKS_PER_SEC;
-    cout << "直接插入排序时间：" << insertionSortTime << " 秒" << endl;
-    printArray(arr, size);
-
-    // 重新生成随机数组
-    for (int i = 0; i < size; i++) {
-        arr[i] = rand() % 2000 + 1;
-    }
+    vector<int> numbersInsertion = numbers;
+    auto startInsertion = high_resolution_clock::now();
+    insertionSort(numbersInsertion);
+    auto stopInsertion = high_resolution_clock::now();
+    auto durationInsertion = duration_cast<microseconds>(stopInsertion - startInsertion);
+    cout << "直接插入排序所用时间（微秒）: " << durationInsertion.count() << endl;
 
     // 希尔排序
-    start = clock();
-    shellSort(arr, size);
-    shellSort(arr, size);
-    finish = clock();
-    double shellSortTime = double(finish - start) / CLOCKS_PER_SEC;
-    cout << "希尔排序时间：" << shellSortTime << " 秒" << endl;
-    printArray(arr, size);
-
-    // 重新生成随机数组
-    for (int i = 0; i < size; i++) {
-        arr[i] = rand() % 2000 + 1;
-    }
+    vector<int> numbersShell = numbers;
+    auto startShell = high_resolution_clock::now();
+    shellSort(numbersShell);
+    auto stopShell = high_resolution_clock::now();
+    auto durationShell = duration_cast<microseconds>(stopShell - startShell);
+    cout << "希尔排序所用时间（微秒）: " << durationShell.count() << endl;
 
     // 冒泡排序
-    start = clock();
-    bubbleSort(arr, size);
-    bubbleSort(arr, size);
-    finish = clock();
-    double bubbleSortTime = double(finish - start) / CLOCKS_PER_SEC;
-    cout << "冒泡排序时间：" << bubbleSortTime << " 秒" << endl;
-    printArray(arr, size);
-
-    // 重新生成随机数组
-    for (int i = 0; i < size; i++) {
-        arr[i] = rand() % 2000 + 1;
-    }
+    vector<int> numbersBubble = numbers;
+    auto startBubble = high_resolution_clock::now();
+    bubbleSort(numbersBubble);
+    auto stopBubble = high_resolution_clock::now();
+    auto durationBubble = duration_cast<microseconds>(stopBubble - startBubble);
+    cout << "冒泡排序所用时间（微秒）: " << durationBubble.count() << endl;
 
     // 快速排序
-    start = clock();
-    quickSort(arr, 0, size - 1);
-    quickSort(arr, 0, size - 1);
-    finish = clock();
-    double quickSortTime = double(finish - start) / CLOCKS_PER_SEC;
-    cout << "快速排序时间：" << quickSortTime << " 秒" << endl;
-    printArray(arr, size);
+    vector<int> numbersQuick = numbers;
+    auto startQuick = high_resolution_clock::now();
+    quickSort(numbersQuick, 0, numbersQuick.size() - 1);
+    auto stopQuick = high_resolution_clock::now();
+    auto durationQuick = duration_cast<microseconds>(stopQuick - startQuick);
+    cout << "快速排序所用时间（微秒）: " << durationQuick.count() << endl;
 
     return 0;
 }
+
